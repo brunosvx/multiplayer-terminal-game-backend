19a1d3ebd57bd90028685cb77a0b8785e43cc2e3
Bruno <mitozgamer137@gmail.com>
2022-09-19 23:08:45 -0300

    create move player and collision check

--- src/server.js
+++ src/server.js
@@ -19,3 +19,45 @@ function removePlayer({ playerId }) {
 function removeFruit({ fruitId }) {
     delete gameState.fruits[fruitId];
 }
+
+function movePlayer({ playerId, move }) {
+    const acceptedMoves = {
+        down() {
+            if(gameState.players[playerId].positionY + 1 === config.screenHeight) return
+            gameState.players[playerId].positionY++
+        },
+        up() {
+            if(gameState.players[playerId].positionY === 0) return
+            gameState.players[playerId].positionY--
+        },
+        left() {
+            if(gameState.players[playerId].positionX === 0) return
+            gameState.players[playerId].positionX--
+        },
+        right() {
+            if(gameState.players[playerId].positionX + 1 === config.screenWidth) return
+            gameState.players[playerId].positionX++
+        },
+    }
+
+    const moveFunction = acceptedMoves[move];
+
+    if(!moveFunction) return;
+
+    moveFunction();
+    checkPlayerCollision({ playerId })
+}
+
+
+function checkPlayerCollision({ playerId }) {
+    const player = gameState.players[playerId];
+
+    for(const fruitId in gameState.fruits){
+        const fruit = gameState.fruits[fruitId];
+        if(player.positionX !== fruit.positionX || player.positionY !== fruit.positionY) continue
+
+        removeFruit({
+            fruitId
+        })
+    }
+}
