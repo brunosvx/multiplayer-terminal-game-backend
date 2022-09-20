edccada8eedea68b43b23184f545ac3a63d6243e
Bruno <mitozgamer137@gmail.com>
2022-09-19 23:25:17 -0300

    split code into two files

---
+++ src/game.js
@@ -0,0 +1,62 @@
+const gameState = {
+    players:{},
+    fruits: {}
+}
+
+export function addPlayer({ playerId, positionX, positionY }) {
+    gameState.players[playerId] = {
+        positionX,
+        positionY
+    }
+    
+}
+
+export function removePlayer({ playerId }) {
+    delete gameState.players[playerId];
+}
+
+function removeFruit({ fruitId }) {
+    delete gameState.fruits[fruitId];
+}
+
+export function movePlayer({ playerId, move }) {
+    const acceptedMoves = {
+        down() {
+            if(gameState.players[playerId].positionY + 1 === config.screenHeight) return
+            gameState.players[playerId].positionY++
+        },
+        up() {
+            if(gameState.players[playerId].positionY === 0) return
+            gameState.players[playerId].positionY--
+        },
+        left() {
+            if(gameState.players[playerId].positionX === 0) return
+            gameState.players[playerId].positionX--
+        },
+        right() {
+            if(gameState.players[playerId].positionX + 1 === config.screenWidth) return
+            gameState.players[playerId].positionX++
+        },
+    }
+
+    const moveFunction = acceptedMoves[move];
+
+    if(!moveFunction) return;
+
+    moveFunction();
+    checkPlayerCollision({ playerId })
+}
+
+
+function checkPlayerCollision({ playerId }) {
+    const player = gameState.players[playerId];
+
+    for(const fruitId in gameState.fruits){
+        const fruit = gameState.fruits[fruitId];
+        if(player.positionX !== fruit.positionX || player.positionY !== fruit.positionY) continue
+
+        removeFruit({
+            fruitId
+        })
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,73 +1,12 @@
 import { Server } from 'socket.io';
 
+import game from './game.js';
+
 const io = new Server(3333, {
     cors: {
         origin: '*'
     }
 })
-
-const gameState = {
-    players:{},
-    fruits: {}
-}
-
-function addPlayer({ playerId, positionX, positionY }) {
-    gameState.players[playerId] = {
-        positionX,
-        positionY
-    }
-    
-}
-
-function removePlayer({ playerId }) {
-    delete gameState.players[playerId];
-}
-
-function removeFruit({ fruitId }) {
-    delete gameState.fruits[fruitId];
-}
-
-function movePlayer({ playerId, move }) {
-    const acceptedMoves = {
-        down() {
-            if(gameState.players[playerId].positionY + 1 === config.screenHeight) return
-            gameState.players[playerId].positionY++
-        },
-        up() {
-            if(gameState.players[playerId].positionY === 0) return
-            gameState.players[playerId].positionY--
-        },
-        left() {
-            if(gameState.players[playerId].positionX === 0) return
-            gameState.players[playerId].positionX--
-        },
-        right() {
-            if(gameState.players[playerId].positionX + 1 === config.screenWidth) return
-            gameState.players[playerId].positionX++
-        },
-    }
-
-    const moveFunction = acceptedMoves[move];
-
-    if(!moveFunction) return;
-
-    moveFunction();
-    checkPlayerCollision({ playerId })
-}
-
-
-function checkPlayerCollision({ playerId }) {
-    const player = gameState.players[playerId];
-
-    for(const fruitId in gameState.fruits){
-        const fruit = gameState.fruits[fruitId];
-        if(player.positionX !== fruit.positionX || player.positionY !== fruit.positionY) continue
-
-        removeFruit({
-            fruitId
-        })
-    }
-}
 
 io.on('connection', (socket) => {
 
