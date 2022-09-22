5a8ab536b9c91818b1128d2e220aa1bd7d4eaee8
Bruno <mitozgamer137@gmail.com>
2022-09-22 15:48:49 -0300

    remove disconnected player

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import { Server } from 'socket.io';
 
-import { addPlayer, movePlayer, gameState } from './game.js';
+import { addPlayer, movePlayer, gameState, removePlayer } from './game.js';
 console.log('server.js')
 const io = new Server(3333, {
     cors: {
@@ -10,6 +10,14 @@ const io = new Server(3333, {
 
 io.on('connection', (socket) => {
     console.log('New socket connected:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Socket disconnected:', socket.id);
+    
+        removePlayer({ playerId: socket.id });
+    
+        io.emit('playerDisconnected', { playerId: socket.id });
+    })
 
     socket.on('move', data => {
        const hasMoved = movePlayer({
